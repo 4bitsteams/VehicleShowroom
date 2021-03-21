33b1f1594f2819b4d8214288ec06f8be0d135289
rubel islam <rubelislam301@gmail.com>
2021-03-22 00:02:03 +0600

    Add EntityBase Class   #1

---
+++ VehicleShowroom/Entity/EntityBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleShowroom.Entity
+{
+    public class EntityBase
+    {
+        //Add common Properties here that will be used for all your entities
+        public long Id { get; set; }
+        public long RowCreatedById { get; set; }
+        public long RowModifiedById { get; set; }
+        public DateTime RowCreatedDateTimeUtc { get; set; }
+        public DateTime RowModifiedDateTimeUtc { get; set; }
+    }
+}
