570bcac49189eb3c0a86a706167f8c33df55292e
rubel islam <rubelislam301@gmail.com>
2021-03-22 00:43:09 +0600

    Add Common Utils and Enum #2

---
+++ VehicleShowroom/Common/Enum/EngineType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleShowroom.Common.Enum
+{
+    public enum EngineType
+    {
+        Oil = 1,
+        Gas = 2,
+        Diesel=3
+    }
+}
