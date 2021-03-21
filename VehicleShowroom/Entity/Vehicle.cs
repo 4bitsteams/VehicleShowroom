a8131a16ef2129d7cd577afa2e2a960341915718
rubel islam <rubelislam301@gmail.com>
2021-03-22 01:25:12 +0600

    Vecle Entity Design Completed  #1

---
+++ VehicleShowroom/Entity/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleShowroom.Common.Enum;
+
+namespace VehicleShowroom.Entity
+{
+    public abstract class Vehicle : EntityBase
+    {
+        private EngineType __EngineType { get; set; }
+
+        public String ModelNumber { get; set; }
+        public EngineType EngineType
+        {
+            get
+            {
+                return __EngineType;
+            }
+            protected set
+            {
+                __EngineType = value;
+            }
+        }
+
+        public Double EnginePower { get; set; }
+        public Double TireSize { get; set; }
+    }
+}
