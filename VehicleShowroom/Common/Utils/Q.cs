e3f604e466de9a32d94ca7fe0179ba38325805da
rubel islam <rubelislam301@gmail.com>
2021-03-22 13:44:37 +0600

    Implement vehicleFactory and GetNextId   Method  #8

--- VehicleShowroom/Common/Utils/Q.cs
+++ VehicleShowroom/Common/Utils/Q.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VehicleShowroom.Entity;
 
 namespace VehicleShowroom.Common.Utils
 {
     public static partial class Q
     {
+        public static long GetNextId(List<Vehicle> entities)
+        {
+            if (entities == null || !(entities.Count > 0))
+                return 1;
+            return entities.Max(x => x.Id) + 1;
+        }
     }
 }
--- VehicleShowroom/Manager/VehicleManager.cs
+++ VehicleShowroom/Manager/VehicleManager.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VehicleShowroom.Common.Enum;
+using VehicleShowroom.Common.Utils;
 using VehicleShowroom.Entity;
+using VehicleShowroom.Factory;
 using VehicleShowroom.Repository;
 
 namespace VehicleShowroom.Manager
@@ -11,6 +14,7 @@ namespace VehicleShowroom.Manager
     public class VehicleManager
     {
         private readonly VehicleRepository vehicleRepository;
+        private readonly VehicleFactory vehicleFactory;
 
         public long Add(Vehicle entity, ref List<Vehicle> entities)
         {
@@ -25,6 +29,16 @@ namespace VehicleShowroom.Manager
         public List<Vehicle> GetAll(ref List<Vehicle> entities)
         {
             return vehicleRepository.GetAll(ref entities);
+        }
+
+        public long GetNextId(List<Vehicle> entities)
+        {
+            return Q.GetNextId(entities);
+        }
+
+        public Vehicle GetVehicle(VehicleType vehicleType)
+        {
+            return vehicleFactory.GetVehicle(vehicleType);
         }
     }
 }
