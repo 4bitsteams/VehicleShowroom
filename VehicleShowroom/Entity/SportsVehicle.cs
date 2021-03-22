1b01f525aef3ea6df2ef969b73bf322d28984b36
rubel islam <rubelislam301@gmail.com>
2021-03-22 11:15:55 +0600

    add Sports Vehicle  Class  #4

---
+++ VehicleShowroom/Entity/SportsVehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleShowroom.Common.Enum;
+namespace VehicleShowroom.Entity
+{
+    public class SportsVehicle : Vehicle
+    {
+        SportsVehicle()
+        {
+            this.EngineType = EngineType.Oil;
+        }
+
+        public dynamic Turbo { get; set; }
+    }
+}
