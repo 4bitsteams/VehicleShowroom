e12baf23477aba42c0a60db7eb1e0635aa8cb38c
rubel islam <rubelislam301@gmail.com>
2021-03-22 11:39:47 +0600

    Design Vehicle  Factory Class #6

---
+++ VehicleShowroom/Factory/VehicleFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleShowroom.Common.Enum;
+using VehicleShowroom.Entity;
+
+namespace VehicleShowroom.Factory
+{
+    public class VehicleFactory
+    {
+        //use GetVehicle method to get object of type Vehicle 
+        public Vehicle GetVehicle(VehicleType vehicleType)
+        {
+            if (!(vehicleType > 0))
+            {
+                return null;
+            }
+            if (vehicleType == VehicleType.Normal)
+            {
+                return new NormalVehicle();
+
+            }
+            else if (vehicleType == VehicleType.Sports)
+            {
+                return new SportsVehicle();
+
+            }
+            else if (vehicleType == VehicleType.Heavy)
+            {
+                return new HeavyVehicle();
+            }
+
+            return null;
+        }
+    }
+}
