89fea8b2c8382e952694bb3a7c2a29b35d17f883
rubel islam <rubelislam301@gmail.com>
2021-03-22 11:19:55 +0600

    Add Heavy Vehicle Class #5

---
+++ VehicleShowroom/Entity/HeavyVehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleShowroom.Common.Enum;
+namespace VehicleShowroom.Entity
+{
+    public class HeavyVehicle : Vehicle
+    {
+        HeavyVehicle()
+        {
+            this.EngineType = EngineType.Diesel;
+        }
+
+        public Double Weight { get; set; }
+    }
+}
