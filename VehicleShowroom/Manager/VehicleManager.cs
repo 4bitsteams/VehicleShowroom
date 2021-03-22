91bf4570444a3166de51de3247da3451074179bd
rubel islam <rubelislam301@gmail.com>
2021-03-22 13:31:44 +0600

    Implement Some Method in VehicleManager Class #8

---
+++ VehicleShowroom/Manager/VehicleManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleShowroom.Entity;
+using VehicleShowroom.Repository;
+
+namespace VehicleShowroom.Manager
+{
+    public class VehicleManager
+    {
+        private readonly VehicleRepository vehicleRepository;
+
+        public long Add(Vehicle entity, ref List<Vehicle> entities)
+        {
+            return vehicleRepository.Add(entity, ref entities);
+        }
+
+        public bool Remove(object id, ref List<Vehicle> entities)
+        {
+            return vehicleRepository.Remove(id, ref entities);
+        }
+
+        public List<Vehicle> GetAll(ref List<Vehicle> entities)
+        {
+            return vehicleRepository.GetAll(ref entities);
+        }
+    }
+}
