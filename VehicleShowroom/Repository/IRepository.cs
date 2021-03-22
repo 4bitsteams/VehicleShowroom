bd464cc7a0a271025462f24932bdb622d91938ab
rubel islam <rubelislam301@gmail.com>
2021-03-22 12:28:58 +0600

    Add Generic Repository #7

---
+++ VehicleShowroom/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleShowroom.Repository
+{
+    public interface IRepository<T>
+    {
+        List<T> GetAll(ref List<T> entities);
+        T GetById(object id, ref List<T> entities);
+        long Add(T entity, ref List<T> entities);
+        bool Update(T entity, ref List<T> entities);
+        bool Remove(object id, ref List<T> entities);
+        bool Exist(object id, ref List<T> entities);
+    }
+}
